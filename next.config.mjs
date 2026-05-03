a18a8d8981f6dda1ec2d9b46568983421a7bcf3e
azuostech <azuos.tech@gmail.com>
2026-05-03 16:55:53 -0300

    fix: redirecionar rota duplicada de reset password

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/auth/reset-password/auth/reset-password',
+        destination: '/auth/reset-password',
+        permanent: false
+      }
+    ];
+  }
 };
 
 export default nextConfig;
